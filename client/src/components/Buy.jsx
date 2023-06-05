163196973436061d2a121ba3c7d75eedd88b68bf
sandeep-prajapatii <sandeeprajapati18>
2023-06-05 23:41:13 +0530

    input value change

--- client/src/components/Buy.jsx
+++ client/src/components/Buy.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import { ethers} from "ethers";
 
 export default function Buy({state}) {
 
+  const [name, setName] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleMsgChange = (event) => {
+    setMsg(event.target.value);
+  };
+
   const buyCoffee =  async (event) => {
     event.preventDefault();
     const { contract } = state;
-    const name = document.querySelector("#name").value;
-    const message = document.querySelector("#message").value;
-    console.log(name, message, contract);
-
     const amount = { value: ethers.utils.parseEther("0.0001") };
-
-    const transaction = await contract.buyCoffee(name, message, amount);
+    const transaction = await contract.buyCoffee(name, msg, amount);
+    setName("")
+    setMsg("")
     await transaction.wait();
-    
+    window.location.reload();
     console.log("Transaction is done");
   };
+
   return (
     <div className='container mx-auto'>
         <form  onSubmit={buyCoffee}>
@@ -25,7 +34,7 @@ export default function Buy({state}) {
             <label className='block'>Name</label>
             <input
               type="text"
-              id="name"
+              onChange={handleNameChange}
               placeholder="Enter Your Name"
               className='border-2 border-red-800 rounded w-2/4 h-10 p-2'
             />
@@ -35,8 +44,8 @@ export default function Buy({state}) {
             <label className='block'>Message</label>
             <input
               type="text"
-              id="message"
               maxLength="100"
+              onChange={handleMsgChange}
               placeholder="Enter Your Message"
               className='border-2 border-red-800 rounded w-full h-10 p-2'
             />
@@ -45,9 +54,6 @@ export default function Buy({state}) {
           <button type="submit" disabled={!state.contract} className='bg-red-800 text-white p-2 rounded w-14'>
             Pay
           </button>
-          
-          
-
         </form>
     </div>
   )
